039be9d6ce8134ba7db067f2875069efa694cbdf
YunYouJun <me@yunyoujun.cn>
2019-08-11 16:13:15 +0800

    feat: add mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+
 import './App.css';
+import ProTip from './Protip';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/ElpsyCN/summer-gadgets">
+        Summer Gadgets
+      </Link>
+      {' - '}
+      <Link color="inherit" href="https://github.com/YunYouJun">
+        YunYouJun
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography align="center" variant="h4" component="h1" gutterBottom>
+          夏日小工具
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
