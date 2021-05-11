8669406b900b84362afc8eb1fb63fd1f668cb7a2
genroot <iherewithmyheart@gmail.com>
2021-05-11 18:55:02 +0800

    ➕ add watermark-dom and ✨ add water mark copyright

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import Container from "@material-ui/core/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Rc from "./pages/Rc";
+import watermark from "watermark-dom";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    watermark.load({
+      watermark_txt: "yunyoujun air-conditioner",
+      watermark_alpha: 0.02,
+    });
+    console.log(
+      "copyright: yunyoujun https://github.com/YunYouJun/air-conditioner"
+    );
+  }, []);
   return (
     <ThemeProvider>
       <Container maxWidth="sm">
---
+++ src/watermark-dom.d.ts
@@ -0,0 +1,1 @@
+declare module "watermark-dom";
