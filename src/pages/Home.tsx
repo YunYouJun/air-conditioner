7ac8747ac3c39f29f31174511b2616d26fadcd31
YunYouJun <me@yunyoujun.cn>
2021-05-12 21:35:36 +0800

    :bug: detect storage to sync state close #32

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,44 +11,15 @@ import Toast from "../features/toast/Toast";
 
 import { ThemeProvider } from "../theme";
 
-import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { useAppSelector } from "../app/hooks";
 import { RootState } from "../app/store";
 import Copyright from "../layouts/Copyright";
-import {
-  AcMode,
-  acItemKey,
-  setTemperature,
-  setMode,
-  setStatus,
-} from "../features/ac/acSlice";
-import { useEffect } from "react";
+import { useDetectStorage } from "../features/ac";
 
 function Home() {
   const ac = useAppSelector((state: RootState) => state.ac);
 
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    function onStorage(e: StorageEvent) {
-      // 重复设置相同的键值不会触发该事件
-      switch (e.key) {
-        case acItemKey.status:
-          dispatch(setStatus(e.newValue === "true"));
-          break;
-        case acItemKey.temperature:
-          dispatch(setTemperature(parseInt(e.newValue || "20")));
-          break;
-        case acItemKey.mode:
-          dispatch(setMode((e.newValue || "cold") as AcMode));
-          break;
-        default:
-          break;
-      }
-    }
-    window.addEventListener("storage", onStorage);
-    return () => {
-      window.removeEventListener("storage", onStorage);
-    };
-  }, [dispatch]);
+  useDetectStorage();
 
   /**
    * 根据模式返回对应的色温
--- src/pages/Rc.tsx
+++ src/pages/Rc.tsx
@@ -1,7 +1,9 @@
 import RemoteControl from "../components/RemoteControl";
 import Copyright from "../layouts/Copyright";
+import { useDetectStorage } from "../features/ac";
 
 export default function Rc() {
+  useDetectStorage();
   return (
     <div>
       <RemoteControl />
